e7d48d907f0b1e0641183a11f0dfef73f64ba49f
Ubuntu <kan.ck.chen@gmail.com>
2019-01-25 04:04:34 +0000

    I am pushing all file to my GitHub  20190124 8:00pm

---
+++ C_Program.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+
+int main(){
+
+printf(“Hello! I am C-Program. Thank you!”);
+
+return 0;
+
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+var texts = “I am your JavaScript Program”;
+
+q
+console.log(texts);
